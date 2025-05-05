23872c114cf6565db4593ce3c23f9dbc9b1d48b4
themkoi <pascalsch3@gmail.com>
2025-05-05 21:03:12 +0200

    noise

--- src/cyd.cpp
+++ src/cyd.cpp
@@ -49,8 +49,8 @@ void updateTimeLabel()
 
 static unsigned long lastValidTouchTime = 0;
 static unsigned long lastTouchStartTime = 0;
-static const unsigned long noiseThreshold = 50; // Minimum duration (ms) to consider as a valid touch
-static const unsigned long debounceDelay = 50;  // Debounce time (ms)
+static const unsigned long noiseThreshold = 80; // Minimum duration (ms) to consider as a valid touch
+static const unsigned long debounceDelay = 100;  // Debounce time (ms)
 static bool lastTouchState = false;
 
 void my_touchpad_read(lv_indev_t *indev, lv_indev_data_t *data)
